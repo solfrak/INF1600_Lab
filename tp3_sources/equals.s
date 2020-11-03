316a77491e5bf95099270318d4356e7a8a03d427
Samuel Laporte <laporte.samuel2000@gmail.com>
2020-11-03 12:17:45 -0500

    equal.s work

--- tp3_sources/equals.s
+++ tp3_sources/equals.s
@@ -2,10 +2,64 @@
 .globl matrix_equals_asm
 
 matrix_equals_asm:
+        /* Programme ici */
+        .data
+        maColonne:  .int 0
+        maRange: .int 0
+        data1: .int 0
+        data2: .int 0
+        matOrder: .int 0
+        .text
+
         push %ebp      /* Save old base pointer */
         mov %esp, %ebp /* Set ebp to current esp */
-        
-        /* Programme ici */
+        pushl %edi
+        LOOP1:
+                movl $0, maColonne
+                jmp LOOP2
+        LOOP2:
 
-        leave          /* Restore ebp and esp */
-        ret            /* Return to the caller */
+                
+                # Assignation Registre
+                movl 8(%ebp), %edx      # inmatdata1
+                movl 12(%ebp), %edi     # inmatdata2
+                movl 16(%ebp), %eax     # matorder
+                movl %eax, matOrder
+
+                # Calculquitr
+                
+                mov maRange, %ecx      # maRange dans %ecx
+                imul %eax, %ecx         # maRange * matOrder dans %ecx
+                addl maColonne, %ecx    # maColonne + %ecx 
+
+                # inmatdata[c+r*matorder]
+                movl (%edx, %ecx, 4), %eax      # inmatdata1[c+r*matorder]
+                movl %eax, data1        # met le résultat dans data1
+                movl (%edi, %ecx, 4), %eax      # inmatdata2[c+r*matorder]
+                movl %eax, data2        # met le résultat dans data2
+
+                # met la valeur data1 dans %eax
+                movl data1, %eax
+                cmp %eax, data2  # si data1 != data2 fait loop2end
+                je LOOP2END
+                # sinon retourne 0 dans %eax et met fin a la boucle
+                movl $0, %eax
+                jmp END1
+        LOOP2END:
+                addl $1, maColonne
+                movl matOrder, %eax
+                cmp maColonne, %eax
+                je LOOP1END
+                jmp LOOP2
+        LOOP1END:
+                addl $1, maRange
+                cmp maRange, %edi
+                je LOOP1
+                jmp END2
+        END2:
+                movl $1, %eax
+                jmp END1
+        END1:
+                popl %edi
+                leave          /* Restore ebp and esp */
+                ret            /* Return to the caller */
--- tp3_sources/transpose.s
+++ tp3_sources/transpose.s
@@ -4,7 +4,47 @@ matrix_transpose_asm:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
+        pushl %ebx
+        pushl %esi
+
 		/* Programme ici */
-		
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+
+        movl $0, %esi   # r
+        movl $0, %edi   # c
+        LOOP1:
+            addl $1, %esi
+            movl $0, %edi
+        LOOP2:
+            addl $1, %edi
+            
+            movl 8(%ebp), %ecx  # inmatdata
+            movl 12(%ebp), %edx  # outmatdata
+            movl 16(%ebp), %ebx  # matorder
+
+            # c+r*matorder dans %eax
+            movl %esi, %eax
+            imul %ebx, %eax
+            addl %edi, %eax
+            # met la valeur inmatdata[c+r*matorder] dans %ecx
+            movl (%ecx, %eax, 4), %ecx
+
+            # r+c*matorder dans %eax
+            movl %edi, %eax
+            imul %ebx, %eax
+            addl %esi, %eax
+            # met la valeur %ecx dans outmatdata[r+c*matorder]
+            movl %ecx, (%edx, %eax, 4)
+        
+        LOOP2END:
+            cmp %esi, %ebx
+            je LOOP1END
+            jmp LOOP2
+        LOOP1END:
+            cmp %esi, %ebx
+            je LOOP1
+            jmp END
+        END:
+            popl %esi
+            popl %ebx
+            leave          /* restore ebp and esp */
+            ret            /* return to the caller */
